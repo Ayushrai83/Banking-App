126b25fbbd23d7bbbc93bafb45d1ea3d8747e3a4
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-13 22:52:16 +0530

    SideBar Setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Ayush", lastName: "Rai" };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
     </main>
   );
 }
