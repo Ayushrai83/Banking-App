e1c6dfe3d65b16d06f134505dbe6702ecce89c85
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-17 12:19:12 +0530

    Finalizing sign-up and sign-in form

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -20,17 +20,15 @@ import { Divide, Loader2 } from "lucide-react";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 
-const formSchema = z.object({
-  email: z.string().email(),
-});
-
 const Authform = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const formSchema = authFormSchema(type);
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -38,7 +36,7 @@ const Authform = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setIsLoading(true);
