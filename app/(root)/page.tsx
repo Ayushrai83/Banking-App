0caf542eb549321c6247cbd5b4d785c2a43573d8
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-20 20:29:36 +0530

    transaction history implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,19 @@ import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 import RightSidebar from "@/components/RightSidebar";
 import { getLoggedInUser } from "@/lib/actions/user.action";
+import { getAccount, getAccounts } from "@/lib/actions/bank.action";
+import RecentTransactions from "@/components/RecentTransactions";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if (!accounts) return;
+  const accountsData  = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData [0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
@@ -14,18 +24,27 @@ const Home = async () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
+            accounts={accountsData }
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT TRANSACTIONS
+        <RecentTransactions
+          accounts={accountsData }
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
-      <RightSidebar user={loggedIn} transaction={[]} banks={[{currentBalance: 123.50},{currentBalance: 500.50}]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData ?.slice(0, 2)}
+      />
     </section>
   );
 };
